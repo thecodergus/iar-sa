5e2b063af9480add26bde2fa26d9388edd481949
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-10-28 12:16:56 -0300

    Refatorando o codigo

--- src/lib/principal.rs
+++ src/lib/principal.rs
@@ -1,7 +1,9 @@
 use rand::{thread_rng, Rng};
 
 use super::cooling_schedule;
+
 use super::vetores::{bit_flip_with_probability, change_for_boolean};
+use core::f64;
 
 #[derive(Debug, Clone)]
 pub struct Output {
@@ -26,73 +28,71 @@ pub fn funcao_objetivo(sat: &Vec<Vec<i32>>, booleanos: &Vec<bool>) -> f64 {
 }
 
 pub fn simulated_annealing(
-    mut s: Vec<bool>,
+    estado_incial: Vec<bool>,
     sat: Vec<Vec<i32>>,
     temperatura_inicial: f64,
     alfa: f64,
     maximo_interacoes: usize,
     fn_temperatura: &dyn Fn(f64, f64, f64, f64) -> f64,
 ) -> (Vec<bool>, Vec<Output>) {
-    let mut s_asterisco: Vec<bool> = s.clone();
-    let mut iter_t: usize = 0;
-    let mut contador: usize = 0;
-    let mut temperatura: f64 = temperatura_inicial;
     let mut historico: Vec<Output> = vec![];
+    let mut estado: Vec<bool> = estado_incial.clone();
+    let mut energia: f64 = funcao_objetivo(&sat, &estado);
+    let mut temperatura: f64 = temperatura_inicial;
+    let mut melhor_estado: Vec<bool> = estado.clone();
+    let mut rng = thread_rng();
 
-    println!("Iniciando Simulated Annealing");
-    println!("Temperatura inicial: {}", temperatura);
-    println!("Solução inicial s: {:?}", s);
-    println!("-------------------------------------");
+    historico.push(Output {
+        fo: energia,
+        interacao: 0,
+        temperatura: temperatura.clone(),
+        trues: somar_trues(&sat, &melhor_estado),
+    });
 
-    while temperatura > 1e-4 {
-        println!(
-            "Temperatura atual: {} | FO: {}",
-            temperatura,
-            funcao_objetivo(&sat, &s_asterisco)
-        );
-        contador += 1;
-        while iter_t < maximo_interacoes {
-            iter_t += 1;
+    for _ in 0..maximo_interacoes {
+        let proximo_estado: Vec<bool> = bit_flip_with_probability(&estado, 5e-2);
+        let nova_energia: f64 = funcao_objetivo(&sat, &proximo_estado);
 
-            let s_linha: Vec<bool> = bit_flip_with_probability(&s, 5e-2);
-            let fo_s_linha: f64 = funcao_objetivo(&sat, &s_linha);
-            let fo_s: f64 = funcao_objetivo(&sat, &s);
-            let delta: f64 = fo_s_linha - fo_s;
+        estado = {
+            let de: f64 = nova_energia - energia;
+            println!("Energia: {}", nova_energia);
 
-            if delta < 0.0 {
-                s = s_linha.clone();
-
-                if fo_s_linha < funcao_objetivo(&sat, &s_asterisco) {
-                    s_asterisco = s_linha.clone();
-                }
+            if de < 0.0 || rng.gen_range(0.0..=1.0) <= f64::consts::E.powf(-de / temperatura) {
+                energia = nova_energia;
+                proximo_estado
             } else {
-                let probabilidade = (-delta / temperatura).exp();
-                let rand_value = thread_rng().gen_range(0.0..=1.0);
-
-                if rand_value < probabilidade {
-                    s = s_linha.clone();
-                }
+                estado
             }
+        };
+
+        if funcao_objetivo(&sat, &melhor_estado) < funcao_objetivo(&sat, &estado) {
+            melhor_estado = estado.clone();
         }
-        // temperatura = cooling_schedule::sete(iter_t as f64, 10.0, 0.001, 5000.0);
+
         temperatura *= alfa;
-        println!("Nova temperatura: {}", temperatura);
-        iter_t = 0;
+
         historico.push(Output {
-            interacao: contador,
-            fo: funcao_objetivo(&sat, &s_asterisco),
-            temperatura,
-            trues: s_asterisco
-                .iter()
-                .filter(|v| **v)
-                .collect::<Vec<&bool>>()
-                .len(),
+            fo: energia,
+            interacao: 0,
+            temperatura: temperatura.clone(),
+            trues: somar_trues(&sat, &melhor_estado),
         });
-        println!("-------------------------------------");
+
+        if temperatura <= 1e-4 {
+            break;
+        }
     }
 
-    println!("Simulated Annealing concluído.");
-    println!("Melhor solução encontrada s_asterisco: {:?}", s_asterisco);
+    return (melhor_estado, historico);
+}
 
-    return (s_asterisco, historico);
+fn somar_trues(sat: &Vec<Vec<i32>>, booleanos: &Vec<bool>) -> usize {
+    return change_for_boolean(sat, booleanos)
+        .iter()
+        .map(|vector| vector.iter().any(|&i| i))
+        .collect::<Vec<bool>>()
+        .iter()
+        .filter(|v| **v)
+        .collect::<Vec<&bool>>()
+        .len();
 }
