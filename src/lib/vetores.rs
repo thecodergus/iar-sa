93097aa1cb82b8872e19c2d6b410afa9da0e986d
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-10-27 22:47:11 -0300

    Mexendo na função de bitflip

--- src/lib/vetores.rs
+++ src/lib/vetores.rs
@@ -19,7 +19,7 @@ pub fn bit_flip_with_probability(vetor: &Vec<bool>, p: f64) -> Vec<bool> {
         .iter()
         .map(|&bit| {
             // Gera um número aleatório entre 0.0 e 1.0 e inverte o bit se for menor que `p`
-            if rng.gen::<f64>() < p {
+            if rng.gen_range(0.0..=1.0) < p {
                 !bit
             } else {
                 bit
